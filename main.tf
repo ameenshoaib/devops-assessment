5a9e6e40283ebf644a12d5facd2d3f3ef5744032
Ameen Shoaib <71146701+ameenshoaib@users.noreply.github.com>
2023-09-05 22:56:23 +0530

    Add files via upload
    terraform RDS files

--- main.tf
+++ main.tf
@@ -2,9 +2,14 @@ provider "aws" {
   region = "ap-south-1"  # Replace with your desired AWS region
 }
 
-resource "aws_instance" "web_server" {
-  ami           = "ami-06f621d90fa29f6d0"  # Specify the appropriate AMI ID
-  instance_type = "t2.micro"  # Choose an instance type
-  subnet_id     = "subnet-0bd894a32ce3ca003"  # Specify the ID of your public subnet
-  key_name      = "web-server-key"  # Replace with your SSH key pair
+resource "aws_db_instance" "wordpress_db" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  username             = "root"
+  password             = "Ayishameen2028"
+  parameter_group_name = "default.mysql5.7"
+  vpc_security_group_ids = ["sg-010a4b8e4c08bb944"]  # Replace with your security group ID
 }
